19855ba80928a0eaf990bed12d57b6c571c8683f
Helgi <helgimagic@yandex.ru>
2024-10-31 14:14:19 +0300

    styles connected

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-import './styles/reset.css';
-import './styles/style.css';
-import './styles/adding-wrapper.css';
+import '#styles/reset.css';
+import '#styles/style.css';
+import '#styles/adding-wrapper.css';
