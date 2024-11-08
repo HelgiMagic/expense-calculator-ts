71dddffe27a6cd38887922a3552e2083aec537fa
Helgi <helgimagic@yandex.ru>
2024-11-08 17:03:53 +0300

    header styles separated

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-import '#styles/reset.css';
-import '#styles/style.css';
-import '#styles/components/UI/form.css';
+import '#styles/reset.css'
+import '#styles/style.css'
+import '#styles/components/UI/form.css'
+import '#styles/components/header.css'
 
-import expenseState from '#state/expense.ts';
+import expenseState from '#state/expense.ts'
 
-console.log(expenseState);
+console.log(expenseState)
